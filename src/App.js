3f1a09c0c4d0154cc562b1a29bc03b0f54bb941a
soul <soulaiman.soulb@gmail.com>
2023-11-04 23:30:45 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,103 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const [res, setRes] = useState([]);
+  const [Index, setIndex] = useState(0);
+
+  const handleChange = () => {
+    if (Index < res.length - 1) {
+      setIndex(Index + 1);
+    } else {
+      // Reset to the beginning if the end is reached
+      setIndex(0);
+    }
+    getData();
+  }
+
+  const getData = async () => {
+    try {
+      const response = await axios.get('https://api.thecatapi.com/v1/images/search?limit=5'); // Increase the limit to load more images
+      setRes(response.data);
+    } catch (error) {
+      console.error('Request Error:', error);
+    }
+  }
+
+
+  
+  {/*
+  useEffect(() => {
+   
+  const input = [1,2,3,4,5];
+
+  const carre= input.map((i)=>{
+    return i ** 2;
+   })
+   console.log(carre)
+
+   
+  const input2 = [1,-2,3,4,-5];
+   const positifs= input2.filter((i)=>{
+    if(i>0)  return true
+   })
+   console.log(positifs)
+   const sum = input2.reduce((i,c)=>{return i+c
+   })
+
+   console.log(sum)
+  
+ 
+   const sumPositif = positifs.reduce((i,c)=>i+c)
+  console.log(sumPositif)
+
+ 
+  let livres=[
+    {id :10,titre : 'POO',auteur :'RAMI',prix :300},
+    {id :11,titre : 'JS ES6',auteur :'FAMI',prix :230},
+    {id :12,titre : 'Algorithme',auteur :'KARIMI',prix :330},
+    {id :13,titre : 'HTMH& CSS',auteur :'RAMI',prix :340}
+  ]
+
+    const titres = livres.map((livre)=>{
+      return livre.titre;
+    })
+
+    const filterTitres = livres.filter((livre)=>{
+      if(livre.auteur === "RAMI"){
+        return true;
+      }
+    })
+    const titresRami = filterTitres.map((titres)=>{
+      return titres.titre;
+    })
+    const livre12 = livres.find((livre)=>{
+      return livre.id === 12 ; 
+    })
+    const totalprix = livres.reduce((total,livre)=>{
+      return   total +livre.prix;
+    },0)
+
+    const mesLivres = [...livres];
+      
+    
+   console.log(titres);
+   console.log(titresRami)
+   console.log(livre12)
+   console.log(totalprix)
+   console.log(mesLivres)
+  }, []); 
+ */} 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handleChange}>Change</button>
+      {res.length > 0 && (
+     
+        <div>
+          <img src={res[Index].url} alt="Cat" />
+        </div>
+      )}
     </div>
   );
 }
